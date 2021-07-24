22bbc209110e07e8653511611c267b39ab19ed1d
Valeriy <mrminival2@mail.ru>
2021-07-24 15:17:50 +0300

    Добавлен прогресс бар

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import "../styles/style.scss"
 
-let position = 0
+// options
 let countShowSlides = 4
-let currentSlide = 0
 let scrollSlides = 2
 let spaceBetween = 40
 
+// helpers
+let position = 0
+let currentSlide = 0
+let currentScroll = 0
+
+// slider
 const slider = document.querySelector(".slider")
 const sliderList = slider.querySelector(".slider__content")
 const slides = slider.querySelectorAll(".slider__slide")
 const slidesCount = slides.length
 const arrowLeft = document.querySelector(".slider-arrow_left")
 const arrowRight = document.querySelector(".slider-arrow_right")
+const progressBar = document.querySelector(".slider-bar__fill")
 
+// calced values
 const widthSlide = slider.clientWidth / countShowSlides
 const widthSlideWithSpaceBetween = widthSlide + spaceBetween / countShowSlides
 const distanceToScroll = widthSlideWithSpaceBetween * scrollSlides
+let countScrolls = Math.ceil((slidesCount - countShowSlides) / scrollSlides)
 
+progressBar.style.width = currentScroll * (100 / countScrolls) + "%"
 
 slides.forEach(slide => {
     slide.style.minWidth = widthSlide - (spaceBetween - spaceBetween / countShowSlides) + "px"
@@ -34,24 +43,25 @@ arrowRight.addEventListener("click", (e) => {
 const swipeLeft = () => {
     // если при перемещении влево осталось переместить меньше слайдов, чем в конфиге
     // то переместить в крайний левый край
-    console.log(currentSlide, scrollSlides)
     if (currentSlide < scrollSlides && currentSlide != 0) {
         currentSlide = 0
         position = 0
+        currentScroll = 0
     // если слайдер в самом левом краю, то передвинуть в крайний правый
     } else if (currentSlide == 0) {
         position = -(slidesCount - countShowSlides) * widthSlideWithSpaceBetween
         currentSlide = slidesCount - countShowSlides
+        currentScroll = countScrolls
     } else {
         position += distanceToScroll
         currentSlide -= scrollSlides
+        currentScroll -= 1
     }
 
     updatePosition()
 }
 
 const swipeRight = () => {
-    console.log(widthSlideWithSpaceBetween)
 
     // если при перемещении вправо осталось переместить меньше слайдов,чем в конфиге и слайдер не находится в крайнем правом положении,
     // то переместить в крайний правый край
@@ -59,13 +69,16 @@ const swipeRight = () => {
     if (currentSlide > slidesLeft && currentSlide != slidesCount - countShowSlides) {
         position = -(slidesCount - countShowSlides) * widthSlideWithSpaceBetween
         currentSlide = slidesCount - countShowSlides
+        currentScroll += 1
     // если слайдер в самом правом краю, то передвинуть в крайний левый
     } else if (currentSlide == slidesCount - countShowSlides) {
         position = 0
         currentSlide = 0
+        currentScroll = 0
     } else {
         position -= distanceToScroll
         currentSlide += scrollSlides
+        currentScroll += 1
     }
 
     updatePosition()
@@ -73,4 +86,6 @@ const swipeRight = () => {
 
 function updatePosition() {
     sliderList.style.transform = `translateX(${position}px)`
+    progressBar.style.width = currentScroll * (100 / countScrolls) + "%"
 }
+
