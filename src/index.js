cb7b884d2a25fbac52c2d313d02110a2e409ef31
Valeriy <mrminival2@mail.ru>
2021-07-24 14:51:37 +0300

    корректный скролл с отступами между слайдами

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import "../styles/style.scss"
 
 let position = 0
 let countShowSlides = 4
-let scrollSlides = 1
+let currentSlide = 0
+let scrollSlides = 2
+let spaceBetween = 40
+
 const slider = document.querySelector(".slider")
 const sliderList = slider.querySelector(".slider__content")
 const slides = slider.querySelectorAll(".slider__slide")
+const slidesCount = slides.length
 const arrowLeft = document.querySelector(".slider-arrow_left")
 const arrowRight = document.querySelector(".slider-arrow_right")
+
 const widthSlide = slider.clientWidth / countShowSlides
-let distanceToScroll = widthSlide * scrollSlides
+const widthSlideWithSpaceBetween = widthSlide + spaceBetween / countShowSlides
+const distanceToScroll = widthSlideWithSpaceBetween * scrollSlides
+
 
 slides.forEach(slide => {
-    slide.style.minWidth = widthSlide + "px"
+    slide.style.minWidth = widthSlide - (spaceBetween - spaceBetween / countShowSlides) + "px"
+    slide.style.marginRight = spaceBetween + "px"
 })
 
 arrowLeft.addEventListener("click", (e) => {
@@ -24,21 +32,45 @@ arrowRight.addEventListener("click", (e) => {
 })
 
 const swipeLeft = () => {
-    if (position == 0) {
-        position = -(slides.length - countShowSlides) * widthSlide
+    // если при перемещении влево осталось переместить меньше слайдов, чем в конфиге
+    // то переместить в крайний левый край
+    console.log(currentSlide, scrollSlides)
+    if (currentSlide < scrollSlides && currentSlide != 0) {
+        currentSlide = 0
+        position = 0
+    // если слайдер в самом левом краю, то передвинуть в крайний правый
+    } else if (currentSlide == 0) {
+        position = -(slidesCount - countShowSlides) * widthSlideWithSpaceBetween
+        currentSlide = slidesCount - countShowSlides
     } else {
         position += distanceToScroll
+        currentSlide -= scrollSlides
     }
 
-    sliderList.style.transform = `translateX(${position}px)`
+    updatePosition()
 }
 
 const swipeRight = () => {
-    if (Math.abs(position) == (slides.length - countShowSlides) * widthSlide) {
+    console.log(widthSlideWithSpaceBetween)
+
+    // если при перемещении вправо осталось переместить меньше слайдов,чем в конфиге и слайдер не находится в крайнем правом положении,
+    // то переместить в крайний правый край
+    const slidesLeft = slidesCount - countShowSlides - scrollSlides
+    if (currentSlide > slidesLeft && currentSlide != slidesCount - countShowSlides) {
+        position = -(slidesCount - countShowSlides) * widthSlideWithSpaceBetween
+        currentSlide = slidesCount - countShowSlides
+    // если слайдер в самом правом краю, то передвинуть в крайний левый
+    } else if (currentSlide == slidesCount - countShowSlides) {
         position = 0
+        currentSlide = 0
     } else {
         position -= distanceToScroll
+        currentSlide += scrollSlides
     }
 
+    updatePosition()
+}
+
+function updatePosition() {
     sliderList.style.transform = `translateX(${position}px)`
 }
